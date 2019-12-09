29d375d19610cf1bd6c1d2e3c8a3eacf7f7045d7
iccy <iccy.fun@outlook.com>
2019-12-09 21:07:59 +0800

    :rocket: Deploying website 2019-12-09 21:07:53

--- run.sh
+++ run.sh
@@ -2,42 +2,26 @@
 
 msg=":rocket: Deploying website `date '+%Y-%m-%d %H:%M:%S'`"
 
-
+# 先是将主目录文件提交上去, 方便两个人修改
 git pull origin master
 git add .
 if [ $# -eq 1 ]
     then msg="$1"
 fi
 	git commit -m "$msg"
-
 git push
 
-
-
-
+# 再将public文件提交到github
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
 # Build the project.
 hugo --theme=m10c --baseUrl="https://ChinaXhm.github.io" --buildDrafts # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-
-
 # Go To Public folder
 cd public
-
 git pull origin master
-
-# Add changes to git.
 git add .
-
-# Commit changes.
-
 if [ $# -eq 1 ]
     then msg="$1"
 fi
 	git commit -m "$msg"
-
-# Push source and build repos.
 git push
-
-# Come Back up to the Project Root
