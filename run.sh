a3c12dd0a00b6ce185238f79f3bf0fd0107ff166
iccy <iccy.fun@outlook.com>
2019-12-09 20:49:45 +0800

    第一次提交

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git pull origin master
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo --theme=m10c --baseUrl="https://ChinaXhm.github.io" --buildDrafts # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg=":rocket: Deploying website `date '+%Y-%m-%d %H:%M:%S'`"
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+	git commit -m "$msg"
+
+# Push source and build repos.
+git push
+
+# Come Back up to the Project Root
