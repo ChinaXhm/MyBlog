9b0cdd44cc728fdc2ee9b88e8e41ecf3d129c142
iccy <iccy.fun@outlook.com>
2019-12-09 21:01:44 +0800

    :rocket: Deploying website 2019-12-09 21:01:39

--- archetypes/default.md
+++ archetypes/default.md
@@ -2,5 +2,7 @@
 title: "{{ replace .Name "-" " " | title }}"
 date: {{ .Date }}
 draft: true
+tags: ["标签", "", ""]
 ---
 
+# 测试一下是否可以提交
--- run.sh
+++ run.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+msg=":rocket: Deploying website `date '+%Y-%m-%d %H:%M:%S'`"
+
+
 git pull origin master
+git add .
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+	git commit -m "$msg"
+
+git push
+
+
+
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo --theme=m10c --baseUrl="https://ChinaXhm.github.io" --buildDrafts # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+
+
 # Go To Public folder
 cd public
+
+git pull origin master
+
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg=":rocket: Deploying website `date '+%Y-%m-%d %H:%M:%S'`"
+
 if [ $# -eq 1 ]
     then msg="$1"
 fi
